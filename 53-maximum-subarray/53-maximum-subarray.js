032aa8530ab4f73e31c42c27a35a6ae2e329faa6
QtMaGIC <qtmagic1992@gmail.com>
2022-07-02 20:50:16 +0800

    Time: 168 ms (6.78%), Space: 50.4 MB (41.87%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    if ( nums.length <= 1){
+        return nums[0]
+    }
+    let fullMax = null
+    let index = 0
+    let current = nums[index]
+    let best = nums[index]
+    index++
+    while(index<nums.length){
+        if ( current + nums[index] > 0 ){
+            current  = current + nums[index] 
+        }else{
+            if ( best >= 0 ){
+                current = 0
+            }
+        }
+        if ( best >= 0 ){
+            if ( current > best){
+                best = current
+            }
+        }else{
+            if ( nums[index] > best){
+                best = nums[index]
+            }
+            if ( nums[index] < 0 ){
+                current = 0
+            }else{
+                current = nums[index]
+            }
+        }
+        //console.log("index: ",index)
+        //console.log("nums[index]: ",nums[index])
+        //console.log("current: ",current)
+        //console.log("best: ",best)
+        index++
+    }
+    return best
+    
+};
