bb12c919e7fb5ecfb99906636fdb03e11b77f973
QtMaGIC <qtmagic1992@gmail.com>
2022-08-04 10:12:37 +0800

    Time: 130 ms (46.38%), Space: 51.8 MB (54.07%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+//[7,1,5,3,6,4]
+// start: 1
+// 1 - 5
+// 1 - 3
+// 1 - 6
+
+// tough caces:
+// [2,1,2,1,0,1,2] - expected: 2
+// [2,4,1] - expected: 2
+var maxProfit = function(prices) {
+	let minPrices = prices[0]
+    let maxProfile = 0
+    for (let i = 0; i< prices.length; i++){
+        const currentPrice = prices[i]
+        const currentProfile = prices[i] - minPrices
+        
+        if ( currentPrice < minPrices  ){
+            //console.log( currentPrice , " ---> ", minPrices)
+            minPrices = currentPrice
+        }
+        
+        if ( currentProfile > maxProfile  ){
+            //console.log( currentProfile , " ---> ", maxProfile)
+            maxProfile = currentProfile
+        }
+        
+    }
+
+	return maxProfile;
+};
