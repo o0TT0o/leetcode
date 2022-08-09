8374e0642d293ba49d799af68a0576460dcd7aaf
QtMaGIC <qtmagic1992@gmail.com>
2022-08-09 19:29:06 +0800

    Time: 106 ms (57.20%), Space: 15.4 MB (9.00%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        tmp = {}
+        index = 0
+        for i in nums:
+            wanted = target-i
+            if i in tmp.keys():
+                return [tmp[i], index]
+            else:
+                tmp[wanted] = index
+            index=index+1
+            #print(tmp)
+        return []
+            
