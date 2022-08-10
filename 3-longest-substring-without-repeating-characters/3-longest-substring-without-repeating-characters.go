9117effb2993e11cee38c0aee6b2b241a297ef92
QtMaGIC <qtmagic1992@gmail.com>
2022-08-10 20:50:18 +0800

    Time: 16 ms (44.22%), Space: 3.1 MB (45.19%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.go
@@ -0,0 +1,36 @@
+func lengthOfLongestSubstring(s string) int {
+    //test cases: 
+    //  "dvdf"
+    longest := 0
+    startIndex := 0
+    endIndex := 0
+    collection := make(map[rune]int)
+    for pos, char := range(s) {
+        if val, ok := collection[char]; ok{
+            //fmt.Println(collection)
+            //fmt.Println(startIndex, endIndex)
+            distance := endIndex - startIndex 
+            //fmt.Println(distance)
+            if distance > longest{
+                //fmt.Println(longest , "------------>" , distance)
+                longest = distance
+            }
+            
+            newStart := val + 1
+            if newStart > startIndex{
+                startIndex = newStart
+            }
+            
+            delete(collection,char)
+        }
+        
+        collection[char] = pos
+        endIndex++
+    }
+    distance := endIndex - startIndex 
+    if distance > longest{
+        //fmt.Println(longest , "+++++++>" , distance)
+        longest = distance
+    }
+    return longest
+}
