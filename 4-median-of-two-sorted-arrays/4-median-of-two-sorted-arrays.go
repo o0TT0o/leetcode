332b1d86e1805451a507d56ec6903f0fc703152e
QtMaGIC <qtmagic1992@gmail.com>
2022-08-11 23:46:50 +0800

    Time: 39 ms (11.11%), Space: 6.8 MB (6.57%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.go
@@ -0,0 +1,51 @@
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    var result float64
+    result = 0
+    loopLen := len(nums1) + len(nums2)
+    
+    index1 := 0
+    index2 := 0
+    mergedArr := []int{}
+    
+    
+    for i := 0 ; i< loopLen; i++ {
+        
+        if index1 >= len(nums1){
+            for index2 < len(nums2){
+                mergedArr = append(mergedArr, nums2[index2])
+                index2++
+            }
+            break
+        }
+        
+        if index2 >= len(nums2){
+            for index1 < len(nums1){
+                mergedArr = append(mergedArr, nums1[index1])
+                index1++
+            }       
+            break
+        }
+        
+        
+        if nums1[index1] < nums2[index2] {
+            mergedArr = append(mergedArr, nums1[index1])
+            index1++
+        }else{
+            mergedArr = append(mergedArr, nums2[index2])
+            index2++
+        }
+        
+    }
+    
+    //fmt.Println(mergedArr)
+    //fmt.Println(math.Mod(float64(loopLen),float64(2)))
+    //fmt.Println(loopLen/2)
+    if  math.Mod(float64(loopLen),float64(2)) == 0{
+        result = float64(mergedArr[loopLen/2-1] + mergedArr[loopLen/2]) /2
+    }else{
+        result = float64(mergedArr[loopLen/2])
+    }
+    
+    
+    return result
+}
