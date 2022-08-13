c5b0f45f36efc723b68f77599630b48b029e8d96
QtMaGIC <qtmagic1992@gmail.com>
2022-08-13 16:21:30 +0800

    Time: 39 ms (49.02%), Space: 2.4 MB (99.39%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.go
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.go
@@ -10,7 +10,6 @@ func longestPalindrome(s string) string {
     result := s[0:1]
     resLen := 1
     length := len(s)
-    charArr := strings.Split(s, "")
     
     for pos, char := range(s) {
         leftIndex := pos -1 
@@ -19,12 +18,12 @@ func longestPalindrome(s string) string {
         
         for {
             
-            for seqFlag && leftIndex >= 0 && charArr[leftIndex] == string(char) {
+            for seqFlag && leftIndex >= 0 && s[leftIndex:leftIndex+1] == string(char) {
                 leftIndex--
             }
             seqFlag = false
             
-            if leftIndex >= 0 && rightIndex <length && charArr[leftIndex] == charArr[rightIndex] {
+            if leftIndex >= 0 && rightIndex <length && s[leftIndex:leftIndex+1] == s[rightIndex:rightIndex+1] {
                 leftIndex--
                 rightIndex++
             }else{
