1225e74d7f98abeb263f1e3041bfaec33ea47098
QtMaGIC <qtmagic1992@gmail.com>
2022-08-14 10:20:48 +0800

    Time: 10 ms (79.19%), Space: 7.7 MB (36.02%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.go
+++ 6-zigzag-conversion/6-zigzag-conversion.go
@@ -6,6 +6,8 @@
 //1
 //"A"
 //2
+//"A"
+//3
 //"ABC"
 //2 
 func convert(s string, numRows int) string {
@@ -13,70 +15,44 @@ func convert(s string, numRows int) string {
         return s
     }
     
-    stringLen := len(s)
+    result :=""
+    steps := 0
+    steps2 := 0
     
-    midNum := numRows - 2
-    partOfZNum := numRows + midNum
-    
-    
-    eachPartColumnSize := 1+midNum
-    baseColumn := (stringLen/partOfZNum) * eachPartColumnSize
-    //fmt.Println(baseColumn)
-    
-    modOfZCount := 0
-    modOfZCount = stringLen%partOfZNum
-    
-    
-    //fmt.Println(modOfZCount)
-    
-    
-    if modOfZCount == 0{
-        baseColumn = baseColumn
-    }else if modOfZCount <= numRows{
-        baseColumn = baseColumn + 1
-    }else{
-        baseColumn = baseColumn + 1 + (modOfZCount - numRows ) 
-    }
-    
-    //fmt.Println(baseColumn)
-
-    //const arryColumn = 1000
-    //const arryRow = 1000
-    //var emptyArray [arryColumn][arryRow]string
-    
-    emptyArray := make([][]string, baseColumn)
-    for i := range emptyArray{
-        emptyArray[i] = make([]string , numRows)
-    }
-    
-    index := 0 
-    for i:=0 ; i<baseColumn ; i++{
-        if i==0 || i%eachPartColumnSize ==0 {
-            for j:=0 ; j < numRows; j++ {
-                emptyArray[i][j] = s[index:index+1]
-                index++
-                if index >= stringLen{
-                    break
-                }
+    for i := 0; i< numRows; i++{
+        //result = result + " | "
+        if i==0 || i==numRows-1 {
+            steps = (numRows - 1)*2
+            for j:=i ; j<len(s); j=j+steps{
+                result = result + s[j:j+1]
             }
+            
         }else{
-            pos := numRows -1 - i%eachPartColumnSize
-            emptyArray[i][pos] = s[index:index+1]
-            index++
-        }
-        if index >= stringLen{
-            break
-        }
-    }
-    //fmt.Println(emptyArray)
-    
-    result := ""
-    for j:=0 ; j< numRows; j++{
-        for i:=0; i < baseColumn; i++{
-            if emptyArray[i][j] != ""{
-                result=result+emptyArray[i][j]
+            steps = (numRows - 1)*2
+            steps2 = (numRows - 1)*2 - 2*i
+            
+            if i >= len(s){
+                break 
+            }
+            
+            result = result + s[i:i+1]
+            j1steps := i
+            j2steps := i
+            
+            for {
+                j2steps = j1steps + steps2
+                
+                if j2steps < len(s){
+                    result = result + s[j2steps:j2steps+1]
+                }else{break}
+                
+                j1steps = j1steps + steps
+                if  j1steps < len(s)  {
+                    result = result + s[j1steps:j1steps+1]
+                }else{break}
             }
         }
+        
     }
     
     
