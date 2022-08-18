173655a7a292b2ea7f7a07516c965a20a62aa4f7
QtMaGIC <qtmagic1992@gmail.com>
2022-08-18 09:28:29 +0800

    Time: 3 ms (78.43%), Space: 3 MB (25.63%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.go
+++ 10-regular-expression-matching/10-regular-expression-matching.go
@@ -25,29 +25,40 @@
 //"aaaaaaaaaaaaab"
 //"a*a*a*a*a*a*a*a*a*a*a*a*b"
 func isMatch(s string, p string) bool {
+    cache := make(map[[2]int]bool)
     sLen := len(s)
     pLen := len(p)
     var match func (sIndex int, pIndex int) bool
     match = func (sIndex int, pIndex int) bool {
+        
+        if val, ok := cache[[2]int{sIndex,pIndex}] ;ok{
+            return val
+        }
+        
         //fmt.Println(s, p, sIndex, pIndex)
         if sIndex >= sLen && pIndex >= pLen{
+            cache[[2]int{sIndex,pIndex}] = true
             return true
         }
         
         if pIndex >= pLen{
+            cache[[2]int{sIndex,pIndex}] = false
             return false
         }
         
         paired := sIndex < sLen && (s[sIndex:sIndex+1] == p[pIndex:pIndex+1] || p[pIndex:pIndex+1]==".")
         
         if (pIndex+1 < pLen) && (p[pIndex+1:pIndex+2] == "*"){
-            return (paired && match(sIndex+1, pIndex)) || match(sIndex,pIndex+2)
+            cache[[2]int{sIndex,pIndex}] = (paired && match(sIndex+1, pIndex)) || match(sIndex,pIndex+2)
+            return cache[[2]int{sIndex,pIndex}]
         }
         
         if paired{
-            return match(sIndex+1, pIndex+1)
+            cache[[2]int{sIndex,pIndex}] = match(sIndex+1, pIndex+1)
+            return cache[[2]int{sIndex,pIndex}]
         }
         
+        cache[[2]int{sIndex,pIndex}] = false
         return false
         
         
